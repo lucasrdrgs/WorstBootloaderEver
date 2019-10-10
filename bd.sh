916cb41353c40df2285d307fd6ea9e8ad41b59e4
Lucas Rodrigues <33637247+lucasrdrgs@users.noreply.github.com>
2019-10-10 16:48:55 -0300

    Update bd.sh

--- bd.sh
+++ bd.sh
@@ -11,4 +11,5 @@ dd if=/dev/zero of=disk.img bs=1024 count=637
 
 dd if=boot.bin of=disk.img conv=notrunc
 dd if=kernel.bin of=disk.img seek=1 conv=notrunc
-cp disk.img ../data.bin
+
+echo "Use QEMU executando disk.img para rodar o kernel num emulador."
